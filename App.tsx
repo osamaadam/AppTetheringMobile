68217ca9ac1f07dbc9bda5d518081e12c5712324
Osama Adam <osamaadam98@gmail.com>
2021-06-07 15:19:29 +0200

    refining the app

--- App.tsx
+++ App.tsx
@@ -1,74 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {useRef} from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-} from 'react-native';
+import React from 'react';
 import {BarCodeReadEvent} from 'react-native-camera';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 
 const App = () => {
   const onRead = async (e: BarCodeReadEvent) => {
     try {
-      console.log(e.data);
-      await fetch(`http://192.168.1.10:4000/send/${e.data}`);
+      const url = `http://192.168.1.10:4000/send/${e.data}`;
+      console.log(url);
+      await fetch(url);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return (
-    <SafeAreaView>
-      <StatusBar />
-      <QRCodeScanner
-        onRead={onRead}
-        reactivate
-        topContent={
-          <Text style={styles.centerText}>
-            Go to{' '}
-            <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
-            your computer and scan the QR code.
-          </Text>
-        }
-        bottomContent={
-          <TouchableOpacity style={styles.buttonTouchable}>
-            <Text style={styles.buttonText}>OK. Got it!</Text>
-          </TouchableOpacity>
-        }
-      />
-    </SafeAreaView>
-  );
+  return <QRCodeScanner onRead={onRead} reactivateTimeout={500} reactivate />;
 };
 
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-  buttonTouchable: {
-    padding: 16,
-  },
-});
 export default App;
