2b8cd284e95caba0b8335b6f3b0a927a2ac0ecf3
Osama Adam <osamaadam98@gmail.com>
2021-06-14 11:18:53 +0200

    dynamically fetching current ip address

--- App.tsx
+++ App.tsx
@@ -5,15 +5,15 @@ import QRCodeScanner from 'react-native-qrcode-scanner';
 const App = () => {
   const onRead = async (e: BarCodeReadEvent) => {
     try {
-      const url = `http://192.168.1.10:4000/send/${e.data}`;
+      const url = new URL(e.data);
       console.log(url);
-      await fetch(url);
+      await fetch(url.href);
     } catch (error) {
       console.error(error);
     }
   };
 
-  return <QRCodeScanner onRead={onRead} reactivateTimeout={500} reactivate />;
+  return <QRCodeScanner onRead={onRead} reactivateTimeout={2000} reactivate />;
 };
 
 export default App;
